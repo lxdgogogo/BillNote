38708673e030ec7bae83341f213e02ab77b062f4
unknown <1636614463@qq.com>
2021-05-12 14:53:54 +0800

    first

---
+++ billproj/Billproj/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string code = textBox2.Text;
+            
+            
+        }
+        public bool insertDB(String sql)
+        {
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                try
+                {
+                    cmd.CommandText = sql;//设置SQL语句 
+                    //cmd.Connection = conn();//调用打开数据库连接方法 
+                    cmd.ExecuteNonQuery();//执行 
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+       // 单击“添加信息”按钮，将够造一条SQL语句（实现添加功能）。然后将这条语句传给insertDB方法，实现数据的添加。
+
+    }
+}
---
+++ billproj/Billproj/WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            string connectionString = "server = 121.196.102.135;port=3306;user=billnote;password=wHyaXdnjFZMkzewK;database=billnote;SslMode=None";
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            try
+            {
+                conn.Open();
+                Console.WriteLine("已经建立连接");
+                string str = "INSERT INTO register values ('lxd','1');";    //sql语句，添加记录
+                MySqlCommand co = new MySqlCommand(str, conn);  //往表内添加记录
+                co = new MySqlCommand(str, conn);			//往表内添加记录（同上）
+                co.ExecuteNonQuery();   //执行sql添加语句      
+            }
+
+            catch (MySqlException e)	//catch捕获异常
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            finally
+            {
+                conn.Close();	//关闭连接
+            }
+
+        }
+    }
+}
