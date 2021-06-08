6bc88e3a6d9a9aef590d2c9ffbb623b25d0d085d
ThankyouTaro <291402210@qq.com>
2021-06-08 20:16:04 +0800

    Edit&Query

--- WindowsFormsApp1/Main.cs
+++ WindowsFormsApp1/Main.cs
@@ -16,7 +16,17 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
-
+        private void Call_calc(string type)
+        {
+            /*
+             * type: label1 type
+             * income: label 3
+             * cost: label 5
+             * total: label 7
+             */
+            Calculate calc = new Calculate(type);
+            calc.ShowDialog();
+        }
         private void Main_Load(object sender, EventArgs e)
         {
 
@@ -31,6 +41,63 @@ namespace WindowsFormsApp1
         {
             UploadBill uploadBill = new UploadBill();
             uploadBill.ShowDialog();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+        /* 下列为合计统计函数 */
+        private void 本日账单ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /*
+             * 显示本日的账务 支出与收入
+             */
+            Call_calc("本日");
+        }
+
+        private void 本周账单ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /*
+             * 显示本周的账务 支出与收入
+             */
+            Call_calc("本周");
+        }
+
+        private void 本月账单ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /*
+             * 显示本月的账务 支出与收入
+             */
+            Call_calc("本月");
+        }
+
+        private void 年度账单ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Call_calc("年度");
+        }
+
+        private void 查询目前总收支ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Call_calc("总收支");
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void 编辑账单ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.ReadOnly = false;
+            button1.Visible = true;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.ReadOnly = true;
+            button1.Visible = false;
         }
     }
 }
--- WindowsFormsApp1/UploadBill.Designer.cs
+++ WindowsFormsApp1/UploadBill.Designer.cs
@@ -33,10 +33,8 @@ namespace WindowsFormsApp1
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.comboBox4 = new System.Windows.Forms.ComboBox();
@@ -46,7 +44,7 @@ namespace WindowsFormsApp1
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button1.Location = new System.Drawing.Point(231, 267);
+            this.button1.Location = new System.Drawing.Point(202, 161);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(80, 40);
             this.button1.TabIndex = 0;
@@ -84,16 +82,6 @@ namespace WindowsFormsApp1
             this.label3.TabIndex = 3;
             this.label3.Text = "收支";
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label4.Location = new System.Drawing.Point(37, 174);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(52, 27);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "备注";
-            // 
             // comboBox1
             // 
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
@@ -119,14 +107,6 @@ namespace WindowsFormsApp1
             this.textBox1.Size = new System.Drawing.Size(116, 25);
             this.textBox1.TabIndex = 6;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(111, 174);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(381, 74);
-            this.richTextBox1.TabIndex = 7;
-            this.richTextBox1.Text = "";
             // 
             // comboBox2
             // 
@@ -232,15 +212,13 @@ namespace WindowsFormsApp1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(523, 329);
+            this.ClientSize = new System.Drawing.Size(523, 222);
             this.Controls.Add(this.comboBox5);
             this.Controls.Add(this.comboBox4);
             this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -258,10 +236,8 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.ComboBox comboBox3;
         private System.Windows.Forms.ComboBox comboBox4;
